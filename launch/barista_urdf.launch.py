0b08ecc964eb83b0a35073923416ae56c08fe981
Manuel Roshardt <manuelroshardt@gmail.com>
2024-04-15 19:00:55 +0000

    Added all launch instructions to barista_urdf.launch.file to launch gazebo, spawn the robot, and run rviz2 from a single file. Removed unecessary files from launch; not needed anymore

--- launch/barista_urdf.launch.py
+++ launch/barista_urdf.launch.py
@@ -1,24 +1,22 @@
 import os
-
+import random
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch_ros.actions import Node
 
-# this is the function launch  system will look for
 def generate_launch_description():
-
     ####### DATA INPUT ##########
     urdf_file = 'barista_robot_model.urdf'
-    #xacro_file = "urdfbot.xacro"
     package_description = "barista_robot_description"
-
     ####### DATA INPUT END ##########
+
     print("Fetching URDF ==>")
     robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
 
     # Robot State Publisher
-
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -30,20 +28,52 @@ def generate_launch_description():
 
     # RVIZ Configuration
     rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'urdf_vis.rviz')
-
-
     rviz_node = Node(
-            package='rviz2',
-            executable='rviz2',
-            output='screen',
-            name='rviz_node',
-            parameters=[{'use_sim_time': True}],
-            arguments=['-d', rviz_config_dir])
-
-    # create and return launch description object
-    return LaunchDescription(
-        [            
-            robot_state_publisher_node,
-            rviz_node
-        ]
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        name='rviz_node',
+        parameters=[{'use_sim_time': True}],
+        arguments=['-d', rviz_config_dir]
     )
+
+    # Gazebo Configuration
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_box_bot_gazebo = get_package_share_directory('barista_robot_description')
+    gazebo_models_path = os.path.join(pkg_box_bot_gazebo, 'models')
+    os.environ['GAZEBO_MODEL_PATH'] = gazebo_models_path
+
+    # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
+        )
+    )
+
+    # Position and orientation
+    position = [0.0, 0.0, 0.2]
+    orientation = [0.0, 0.0, 0.0]
+
+    # Base Name or robot
+    robot_base_name = "barista_robot"
+    entity_name = robot_base_name + "-" + str(int(random.random() * 100000))
+
+    # Spawn ROBOT Set Gazebo
+    spawn_robot = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        name='spawn_entity',
+        output='screen',
+        arguments=['-entity', entity_name,
+                   '-x', str(position[0]), '-y', str(position[1]), '-z', str(position[2]),
+                   '-R', str(orientation[0]), '-P', str(orientation[1]), '-Y', str(orientation[2]),
+                   '-topic', '/robot_description']
+    )
+
+    # Create and return launch description object
+    return LaunchDescription([
+        robot_state_publisher_node,
+        rviz_node,
+        gazebo,
+        spawn_robot
+    ])
