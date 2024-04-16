ff6759519f647f992992e6403e98d9fb6d3b048d
Manuel Roshardt <manuelroshardt@gmail.com>
2024-04-16 00:28:30 +0000

    Added gazebo related tags into xacro file and updated launch file

--- launch/barista_xacro.launch.py
+++ launch/barista_xacro.launch.py
@@ -1,16 +1,27 @@
 import os
+import random
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_prefix
+from launch.substitutions import Command
 import xacro
 
 def generate_launch_description():
+    urdf_file = 'barista_robot_model.urdf'
+    package_description = "barista_robot_description"
+
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_box_bot_gazebo = get_package_share_directory('barista_robot_description')
+    gazebo_models_path = os.path.join(pkg_box_bot_gazebo, 'models')
+    os.environ['GAZEBO_MODEL_PATH'] = gazebo_models_path
+
+    # Gazebo launch
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
+        ),
         launch_arguments={"verbose": "false", 'pause': 'true'}.items(),
     )
 
@@ -18,27 +29,64 @@ def generate_launch_description():
         get_package_share_directory('barista_robot_description'))
     xacro_file = os.path.join(robot_model_path, 'xacro', 'barista_robot_model.urdf.xacro')
 
-    # convert XACRO file into URDF
+    # Convert XACRO file into URDF
     doc = xacro.parse(open(xacro_file))
     xacro.process_doc(doc)
     params = {'robot_description': doc.toxml()}
 
-    robot_state_publisher = Node(
+    robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
+
+    # Robot State Publisher
+    robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        output='screen',
-        parameters=[params]
+        name='robot_state_publisher_node',
+        emulate_tty=True,
+        parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', robot_desc_path])}],
+        output="screen"
     )
 
-    spawn_entity = Node(
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher_node'
+    )
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'urdf_vis.rviz')
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        name='rviz_node',
+        parameters=[{'use_sim_time': True}],
+        arguments=['-d', rviz_config_dir]
+    )
+
+    # Position and orientation
+    position = [0.0, 0.0, 0.1107]
+    orientation = [0.0, 0.0, 0.0]
+
+    # Base Name or robot
+    robot_base_name = "barista_robot"
+    entity_name = robot_base_name + "-" + str(int(random.random() * 100000))
+
+    # Spawn ROBOT Set Gazebo
+    spawn_robot = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=['-entity', 'barista_robot', '-x', '0.0', '-y', '0.0', '-z', '0.1107', '-topic', 'robot_description'],
-        output='screen'
+        name='spawn_entity',
+        output='screen',
+        arguments=['-entity', entity_name,
+                   '-x', str(position[0]), '-y', str(position[1]), '-z', str(position[2]),
+                   '-R', str(orientation[0]), '-P', str(orientation[1]), '-Y', str(orientation[2]),
+                   '-topic', '/robot_description']
     )
 
     return LaunchDescription([
         gazebo,
-        robot_state_publisher,
-        spawn_entity,
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        spawn_robot,
+        rviz_node
     ])
