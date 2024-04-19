c74ecd5ddeac2e197ceaf026ccc87c695481a17f
Manuel Roshardt <manuelroshardt@gmail.com>
2024-04-19 04:39:19 +0000

    Fixed single xacro robot launch; macros were causing errors on launch, got rid of inconsistencies in the cup_holder_tray, extracted gazebo tags to gazebo_elements macro, fixed launch file

--- launch/barista_xacro.launch.py
+++ launch/barista_xacro.launch.py
@@ -1,45 +1,45 @@
 import os
-import random
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch.substitutions import Command
+from ament_index_python.packages import get_package_prefix
+
 import xacro
 
-def generate_launch_description():
-    urdf_file = 'barista_robot_model.urdf'
-    package_description = "barista_robot_description"
 
-    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
-    pkg_box_bot_gazebo = get_package_share_directory('barista_robot_description')
-    gazebo_models_path = os.path.join(pkg_box_bot_gazebo, 'models')
-    os.environ['GAZEBO_MODEL_PATH'] = gazebo_models_path
+def generate_launch_description():
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+    )
 
     robot_model_path = os.path.join(
         get_package_share_directory('barista_robot_description'))
+
     xacro_file = os.path.join(robot_model_path, 'xacro', 'barista_robot_model.urdf.xacro')
 
-    # Convert XACRO file into URDF
+    # convert XACRO file into URDF
     doc = xacro.parse(open(xacro_file))
     xacro.process_doc(doc)
     params = {'robot_description': doc.toxml()}
 
-    robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
-
-    # Robot State Publisher
-    robot_state_publisher_node = Node(
+    robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        name='robot_state_publisher_node',
-        emulate_tty=True,
-        parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', robot_desc_path])}],
-        output="screen"
+        output='screen',
+        parameters=[params]
     )
 
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-entity', 'barista_robot', '-x', '0.0', '-y', '0.0', '-z', '0.1107',
+                                   '-topic', 'robot_description'],
+                        output='screen')
+
     # RVIZ Configuration
-    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'urdf_vis.rviz')
+    rviz_config_dir = os.path.join(get_package_share_directory('barista_robot_description'), 'rviz', 'urdf_vis.rviz')
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
@@ -49,36 +49,9 @@ def generate_launch_description():
         arguments=['-d', rviz_config_dir]
     )
 
-    # Gazebo launch
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
-        )
-    )
-
-    # Position and orientation
-    position = [0.0, 0.0, 0.1107]
-    orientation = [0.0, 0.0, 0.0]
-
-    # Base Name or robot
-    robot_base_name = "barista_robot"
-    entity_name = robot_base_name + "-" + str(int(random.random() * 100000))
-
-    # Spawn ROBOT Set Gazebo
-    spawn_robot = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        name='spawn_entity',
-        output='screen',
-        arguments=['-entity', entity_name,
-                   '-x', str(position[0]), '-y', str(position[1]), '-z', str(position[2]),
-                   '-R', str(orientation[0]), '-P', str(orientation[1]), '-Y', str(orientation[2]),
-                   '-topic', '/robot_description']
-    )
-
-    return LaunchDescription([   
-        robot_state_publisher_node,
-        rviz_node,
+    return LaunchDescription([
         gazebo,
-        spawn_robot,
+        rviz_node,
+        robot_state_publisher,
+        spawn_entity,
     ])
