d09fb1b419c9c9d1009fb89a322b01f956e6fef5
Manuel Roshardt <manuelroshardt@gmail.com>
2024-04-19 05:01:13 +0000

    Added launch file that starts gazebo and spawns two robots (still need to change the name to rick and morty), and updated xacro files to add robot_name as parameter; gazebo and topics look as expected

---
+++ launch/barista_two_robots.launch.py
@@ -0,0 +1,97 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_prefix
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    # Declare a new launch argument for the world file
+    world_file_arg = DeclareLaunchArgument(
+        'world',
+        default_value=[get_package_share_directory(
+            'barista_robot_description'), '/worlds/barista_robot_empty.world'],
+        description='Path to the Gazebo world file'
+    )
+
+    # Define the launch arguments for the Gazebo launch file
+    gazebo_launch_args = {
+        'verbose': 'false',
+        'pause': 'false',
+        'world': LaunchConfiguration('world')
+    }
+
+   # Include the Gazebo launch file with the modified launch arguments
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        launch_arguments=gazebo_launch_args.items(),
+    )
+
+    # Define the robot model files to be used
+    robot_desc_file = "barista_robot_model.urdf.xacro"
+    robot_desc_path = os.path.join(get_package_share_directory(
+        "barista_robot_description"), "xacro", robot_desc_file)
+
+    robot_name_1 = "robot1"
+    robot_name_2 = "robot2"
+
+    rsp_robot1 = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        namespace=robot_name_1,
+        parameters=[{'frame_prefix': robot_name_1+'/', 'use_sim_time': use_sim_time,
+                     'robot_description': Command(['xacro ', robot_desc_path, ' robot_name:=', robot_name_1])}],
+        output="screen"
+    )
+
+    rsp_robot2 = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        namespace=robot_name_2,
+        parameters=[{'frame_prefix': robot_name_2+'/', 'use_sim_time': use_sim_time,
+                     'robot_description': Command(['xacro ', robot_desc_path, ' robot_name:=', robot_name_2])}],
+        output="screen"
+    )
+
+    spawn_robot1 = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'robot1', '-x', '0.0', '-y', '0.0', '-z', '0.1107',
+                   '-topic', robot_name_1+'/robot_description']
+    )
+
+    spawn_robot2 = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'robot2', '-x', '1.0', '-y', '1.0', '-z', '0.1107',
+                   '-topic', robot_name_2+'/robot_description']
+    )
+
+    # RVIZ Configuration
+    # rviz_config_dir = os.path.join(get_package_share_directory('barista_robot_description'), 'rviz', 'urdf_vis.rviz')
+    # rviz_node = Node(
+    #     package='rviz2',
+    #     executable='rviz2',
+    #     output='screen',
+    #     name='rviz_node',
+    #     parameters=[{'use_sim_time': True}],
+    #     arguments=['-d', rviz_config_dir]
+    # )
+
+    return LaunchDescription([
+        world_file_arg,
+        gazebo,
+        # rviz_node,
+        rsp_robot1,
+        rsp_robot2,
+        spawn_robot1,
+        spawn_robot2
+    ])
